ed76a147aac6c7fc0f1e88406c371b1507547778
Stormshaper455 <joshuawilson214@gmail.com>
2026-04-08 14:26:48 -0400

    Added width/height pixel calculation

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -16,5 +16,13 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo "Enter width"
+read width
+echo "Enter height"
+read height
+pixel=$(( width*height))
+
+echo "You have $pixel pixels"
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
