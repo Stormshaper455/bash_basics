fb9a3e8cdccab62433422d7859f631dbc9239b9c
Stormshaper455 <joshuawilson214@gmail.com>
2026-04-08 14:39:30 -0400

    Edited 02-add_nums.sh differently on parallel branch

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -11,5 +11,12 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo "Type the width RIGHT NOW"
+read width
+echo "Type the height RIGHT NOW"
+read height
+pixels=$(( width*height ))
+echo "Total pixels: $pixels"
+
 # exercise: ask the user for the width and height and present total
 # number of pixels
